abb26c16c9e10c3dd7f014a3847adaad1323c521
gaojingyi <gaojingyi924@outlook.com>
2021-04-09 22:15:24 +0100

    modify the test code

--- TestArithmetic/src-gen/TestArithmetics.java
+++ TestArithmetic/src-gen/TestArithmetics.java
@@ -2,11 +2,13 @@ public class TestArithmetics{
 		    public static void main(String[] args){
 				System.out.println(5+2);
 				System.out.println(5.6-3.3);
-				System.out.println(4.6/2);
+				System.out.println(4.6/0);
+				System.out.println(2+(2.1*2.1*2.1));
 				System.out.println(3*9.9);
 				System.out.println(4*4*4);
 				System.out.println(((1+2)-2)*((1+2)-2)*((1+2)-2));
 				System.out.println((7*1.1)+((2*2*2)-3));
 				System.out.println((4*4)/2);
+				System.out.println(2*2*2);
 		    }
 }
